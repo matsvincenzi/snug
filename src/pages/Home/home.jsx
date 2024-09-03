182474b9c777ca0eba28ac90e082fa11ff3d61c9
matsvincenzi <matsvincenzi@gmail.com>
2024-09-02 23:59:04 -0300

    feat: starting the homepage

---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,16 @@
+import { Header } from '../../components/Header/header'
+import { Footer } from '../../components/Footer/footer'
+import { PageContainer } from './style'
+
+const Home = () => {
+  return (
+    <>
+      <PageContainer>
+        <Header />
+        <Footer />
+      </PageContainer>
+    </>
+  )
+}
+
+export default Home
