72c97e7f42e23fc64898eeedc4b4f8e8b90980a2
matsvincenzi <matsvincenzi@gmail.com>
2024-09-03 23:53:05 -0300

    feat: welcome section for homepage

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,12 +1,46 @@
 import { Header } from '../../components/Header/header'
 import { Footer } from '../../components/Footer/footer'
-import { PageContainer } from './style'
+import {
+  PageContainer,
+  MainContent,
+  WelcomeSection,
+  WelcomeContainer,
+  TitleContainer,
+  Title,
+  InfoContainer,
+  TitleDescription,
+  ContainerButtons,
+  Buttons,
+} from './style'
 
 const Home = () => {
   return (
     <>
       <PageContainer>
         <Header />
+
+        <MainContent>
+          <WelcomeSection>
+            <WelcomeContainer>
+              <TitleContainer>
+                <Title>Welcome To Snug</Title>
+              </TitleContainer>
+              <InfoContainer>
+                <TitleDescription>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  do eiusmod tempor incididunt ut labore et dolore magna
+                  aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Sed do eiusmo.
+                </TitleDescription>
+                <ContainerButtons>
+                  <Buttons>Lorem Ipsum</Buttons>
+                  <Buttons>Lorem Ipsum</Buttons>
+                </ContainerButtons>
+              </InfoContainer>
+            </WelcomeContainer>
+          </WelcomeSection>
+        </MainContent>
+
         <Footer />
       </PageContainer>
     </>
