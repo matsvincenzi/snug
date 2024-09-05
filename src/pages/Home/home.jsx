ff8c356dfeb00b1faf4358c3e2c94d7d2ecfeb41
matsvincenzi <matsvincenzi@gmail.com>
2024-09-05 03:48:28 -0300

    feat: contacts section for homepage

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,5 +1,10 @@
 import { Header } from '../../components/Header/header'
 import { Footer } from '../../components/Footer/footer'
+import { MdEmail, MdChat, MdPhone } from 'react-icons/md'
+
+import SmallTitle from '../../components/Title/SmallTitle/smallTitle'
+import SmallSubTitle from '../../components/SubTitle/SmallSubTitle/smallSubTitle'
+
 import {
   PageContainer,
   MainContent,
@@ -11,6 +16,11 @@ import {
   TitleDescription,
   ContainerButtons,
   Buttons,
+  ContactSection,
+  ContactContainer,
+  ContactBox,
+  IconContact,
+  ContactLink,
 } from './style'
 
 const Home = () => {
@@ -39,6 +49,44 @@ const Home = () => {
               </InfoContainer>
             </WelcomeContainer>
           </WelcomeSection>
+
+          <ContactSection>
+            <ContactContainer>
+              <ContactBox>
+                <IconContact>
+                  <MdEmail />
+                </IconContact>
+                <SmallTitle>Email</SmallTitle>
+                <SmallSubTitle margin-top="10px">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse varius enim in ero.
+                </SmallSubTitle>
+                <ContactLink href="">loremipsum@gmail.com</ContactLink>
+              </ContactBox>
+              <ContactBox>
+                <IconContact>
+                  <MdChat />
+                </IconContact>
+                <SmallTitle>Live Chat</SmallTitle>
+                <SmallSubTitle>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse varius enim in ero.
+                </SmallSubTitle>
+                <ContactLink href="">Lorem ipsum dolor sit</ContactLink>
+              </ContactBox>
+              <ContactBox>
+                <IconContact>
+                  <MdPhone />
+                </IconContact>
+                <SmallTitle>Phone</SmallTitle>
+                <SmallSubTitle>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse varius enim in ero.
+                </SmallSubTitle>
+                <ContactLink href="">+1 (555) 000-0000</ContactLink>
+              </ContactBox>
+            </ContactContainer>
+          </ContactSection>
         </MainContent>
 
         <Footer />
