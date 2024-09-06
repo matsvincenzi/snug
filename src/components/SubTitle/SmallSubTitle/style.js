6dd3e5deebd1474921029d43a2755d53f856b3bf
matsvincenzi <matsvincenzi@gmail.com>
2024-09-06 20:13:29 -0300

    refactor: adding styles to small subtitle props

--- src/components/SubTitle/SmallSubTitle/style.js
+++ src/components/SubTitle/SmallSubTitle/style.js
@@ -4,5 +4,7 @@ export const StyledSmallSubTitle = styled.p`
   font-size: 16px;
   color: ${({ color }) => color || 'var(--greyDefault)'};
   margin: ${({ margin }) => margin || '0'};
-  line-height: 1.3;
+  text-align: left;
+  max-width: ${({ maxWidth }) => maxWidth || 'none'};
+  line-height: 1.5;
 `
