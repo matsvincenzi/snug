c564f74e7211b5b7194d9395e516f61e036843ad
matsvincenzi <matsvincenzi@gmail.com>
2024-09-06 20:07:23 -0300

    refactor: adding props to the small subtitle

--- src/components/SubTitle/SmallSubTitle/smallSubTitle.jsx
+++ src/components/SubTitle/SmallSubTitle/smallSubTitle.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { StyledSmallSubTitle } from './style'
 
-const SmallSubTitle = ({ children, color, margin }) => (
-  <StyledSmallSubTitle color={color} margin={margin}>
+const SmallSubTitle = ({ children, color, margin, maxWidth }) => (
+  <StyledSmallSubTitle color={color} margin={margin} maxWidth={maxWidth}>
     {children}
   </StyledSmallSubTitle>
 )
