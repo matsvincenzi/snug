77f2688afdbddbd8fe6c0388d24f9a8ae98af50e
matsvincenzi <matsvincenzi@gmail.com>
2024-09-14 20:34:08 -0300

    feat: creating a home page button component

---
+++ src/pages/Home/components/Buttons/buttons.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ContainerButtons, Buttons } from './style'
+
+export const ButtonGroup = () => {
+  return (
+    <ContainerButtons>
+      <Buttons>Lorem Ipsum</Buttons>
+      <Buttons>Lorem Ipsum</Buttons>
+    </ContainerButtons>
+  )
+}
